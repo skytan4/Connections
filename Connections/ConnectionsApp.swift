f7dda2b241155926b31a61a1b8b89b58e7300199
John Tanner <tanner3068@yahoo.com>
2026-04-15 15:00:43 -0700

    Initial Commit

---
+++ Connections/ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsApp.swift
+//  Connections
+//
+//  Created by John Tanner on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connections/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Connections
+//
+//  Created by John Tanner on 4/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
